0760f2ebc2218343377703b357aad5690d8579d9
Robert B <robertb@lifesciencepublishers.com>
2021-11-05 13:32:09 -0600

    add server functions

--- js/ajax.js
+++ js/ajax.js
@@ -13,4 +13,39 @@ xhr.onload = function () {
 
 xhr.send(`Fname=${fname}&Lname=${lname}`);
 
+async function getUsers() {
+    let res = await fetch('https://randomuser.me/api/?results=10');
+    let data = await res.json()
+    console.log(res);
+}
+
+async function setPost() {
+    let _data = {
+        title: "foo",
+        body: "bar", 
+        userId:1
+      }
+    let  req = await fetch('https://jsonplaceholder.typicode.com/posts',
+    {
+        method: 'POST',
+        body: JSON.stringify(_data),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    });
+    let res = await req.json()
+    console.log(res);
+}
+
+setPost();
+
+// fetch(url, {
+//     method: "get", //put your method
+//     headers: {
+//       "Content-Type": "application/json",
+//       "X-Requested-With": "XMLHttpRequest",
+//       "Access-Control-Allow-Origin": "*"
+//     },
+//     mode: 'no-cors'
+//   });
+  
+  //Note: works fine in chrome browsers
 
